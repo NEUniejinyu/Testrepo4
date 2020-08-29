575bf0a3bbb96b646838ab696d31bdebc81675d4
NJY <849299587@qq.com>
2020-08-29 21:53:31 +0800

    第一次提交工程

---
+++ FirstGitTest/src/com/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("1111");
+        System.out.println("2222");
+    }
+}
